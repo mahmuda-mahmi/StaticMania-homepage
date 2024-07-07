3b1d95dfef0b83a1b9a8c17f6c2bd709f3052d36
mahmuda-mahmi <mahmudapakter@gmail.com>
2024-07-07 23:46:12 +0600

    homepage complete

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+$('.responsive').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  nextArrow: '<button type="button" class="slick-next"><i class="fa-thin fa-arrow-right-long"></i></button>',
+  prevArrow: '<button type="button" class="slick-prev"><i class="fa-thin fa-arrow-left-long"></i></button>',
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
